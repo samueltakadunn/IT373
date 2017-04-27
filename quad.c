c8173b8d9ae30e71d6eb486646afba0f783785ab
Samuel Dunn <sdunn95@sbcglobal.net>
2017-04-27 02:05:27 +0000

    first commit

---
+++ quad.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+    {
+        int num = 123;
+        int sum = 0;
+        sum = sum + num;
+        sum = sum + num;
+        sum = sum + num;
+        sum = sum + num;
+        printf("%d\n", sum);
+        return 0;
+    }
